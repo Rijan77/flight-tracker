55712f45aac761344ceba7f4103ae6d170151d53
Rijan77 <rijanacharya73@gmail.com>
2025-01-21 22:23:36 +0545

    Add one more API

--- lib/Static.dart
+++ lib/Static.dart
@@ -6,6 +6,7 @@ class ApiService {
   static const String apiUrl2 = 'https://api.aviationstack.com/v1/flights?access_key=a3e2b1ce7990a9b80dd58876e402b258';
 
   static Future<List<dynamic>> fetchFlights() async {
+
     try {
       final responses = await Future.wait([
       http.get(Uri.parse(apiUrl)),
