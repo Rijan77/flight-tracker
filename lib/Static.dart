3c6504c17a342cc9c4ba5db6e214461db5b1af74
Rijan77 <rijanacharya73@gmail.com>
2025-02-09 07:49:03 +0545

    Update API key

--- lib/Static.dart
+++ lib/Static.dart
@@ -2,15 +2,15 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class ApiService {
-  static const String apiUrl = 'https://api.aviationstack.com/v1/flights?access_key=a3e2b1ce7990a9b80dd58876e402b258';
-  static const String apiUrl2 = 'https://api.aviationstack.com/v1/flights?access_key=a3e2b1ce7990a9b80dd58876e402b258';
+  static const String apiUrl = 'https://api.aviationstack.com/v1/flights?access_key=d86d77166fba00bcac0ec146b6ff9522';
+  // static const String apiUrl2 = 'https://api.aviationstack.com/v1/flights?access_key=d86d77166fba00bcac0ec146b6ff9522';
 
   static Future<List<dynamic>> fetchFlights() async {
 
     try {
       final responses = await Future.wait([
       http.get(Uri.parse(apiUrl)),
-      http.get(Uri.parse(apiUrl2)),
+      // http.get(Uri.parse(apiUrl2)),
     ]);
 
       List<dynamic> flights =[];
