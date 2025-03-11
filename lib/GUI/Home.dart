1eb842e6765f39943026dedfe7a6d3baa7d9ca44
Rijan77 <rijanacharya73@gmail.com>
2025-03-11 08:06:56 +0545

    Update homepage

--- lib/GUI/Home.dart
+++ lib/GUI/Home.dart
@@ -14,6 +14,7 @@ class _HomeState extends State<Home> {
   String _selectedClass = "Economy";
   final List<String> _travelClasses = ["Economy", "Business", "First Class"];
 
+
   int _selectedPeople = 1;
   final List<int> _numPeople = [1, 2, 3, 4];
 
