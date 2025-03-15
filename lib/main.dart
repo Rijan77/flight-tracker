2166820dc7ce09af09900c1935e367c88de9313a
Rijan77 <rijanacharya73@gmail.com>
2025-03-15 21:18:31 +0545

    Modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'GUI/Home.dart';
-import 'GUI/Splash_Screen.dart';
 
 
 Future<void> main() async {
